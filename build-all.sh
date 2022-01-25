84e7ea3f87ec7a6736a50009a55e2581f64a56cb
Jake Tronge <jtronge3@gmail.com>
2022-01-25 12:58:38 -0500

    Update dockerfiles and build scripts

--- build-all.sh
+++ build-all.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-VERSIONS="tags/v6.8-release tags/v7.0.0-release tags/v7.0.1-release tags/v7.0.2-release"
-# VERSIONS="1411fef7b73acebb9451b0345fcd3fd8159d20a3"
+VERSIONS="519b710bd09ebe3c10703293093d3cf70ce44333 adab52acbbc0fb65b35856abf53937360bb52026 a4d514a59c8014fe60b70c1aec0b494ee654aad3 2afe98c0a9f842cdad0b1bc25aedb1fd66ac1db0 35efa19ca7662489b7388c4a00b16645d73ccc48 33d9b4c97c7b16a10b49a00b50aba1a7520e95a9 d68c367218f4fb55d37b89cefc7fd13438588f6e 43193ef55866f3ea99789325529906500c256e82"
 
-cd nwchem-tce-ccsd
+# cd nwchem-tce-ccsd
+cd nwchem
 for version in $VERSIONS; do
-	tag=jtronge/nwchem:`echo $version | cut -d'/' -f2`
+	# tag=jtronge/nwchem:`echo $version | cut -d'/' -f2`
 	# tag=jtronge/nwchem:$version
+	tag=jtronge/nwchem:$version
 	ch-image build --force -t $tag --build-arg VERSION=$version .
 	ch-image push $tag
-	# ch-image reset
+	ch-image reset
 done
--- ci-build.sh
+++ ci-build.sh
@@ -3,9 +3,9 @@
 # Install Charliecloud
 CWD=`pwd`
 cd /tmp
-curl -O -L https://github.com/hpc/charliecloud/releases/download/v0.25/charliecloud-0.25.tar.gz
-tar -xvf charliecloud-0.25.tar.gz
-cd charliecloud-0.25
+curl -O -L https://github.com/hpc/charliecloud/releases/download/v0.26/charliecloud-0.26.tar.gz
+tar -xvf charliecloud-0.26.tar.gz
+cd charliecloud-0.26
 ./configure --prefix=$HOME || exit 1
 make || exit 1
 make install || exit 1
