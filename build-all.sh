0093f1bbb4a7671c72ff2cbd3a2b9d1c850786eb
Jake Tronge <jtronge3@gmail.com>
2022-01-27 09:33:19 -0500

    Update versions to compile

--- build-all.sh
+++ build-all.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-VERSIONS="519b710bd09ebe3c10703293093d3cf70ce44333 adab52acbbc0fb65b35856abf53937360bb52026 a4d514a59c8014fe60b70c1aec0b494ee654aad3 2afe98c0a9f842cdad0b1bc25aedb1fd66ac1db0 35efa19ca7662489b7388c4a00b16645d73ccc48 33d9b4c97c7b16a10b49a00b50aba1a7520e95a9 d68c367218f4fb55d37b89cefc7fd13438588f6e 43193ef55866f3ea99789325529906500c256e82"
+VERSIONS="519b710bd09ebe3c10703293093d3cf70ce44333 05aafc87223af82f58865d8b0f924dabd1adacbc f29685d5bde59ac9c7f71b7ce6193720aaba1a8b"
 
 # cd nwchem-tce-ccsd
 cd nwchem
@@ -7,7 +7,7 @@ for version in $VERSIONS; do
 	# tag=jtronge/nwchem:`echo $version | cut -d'/' -f2`
 	# tag=jtronge/nwchem:$version
 	tag=jtronge/nwchem:$version
-	ch-image build --force -t $tag --build-arg VERSION=$version .
-	ch-image push $tag
-	ch-image reset
+	ch-image build --force -t $tag --build-arg VERSION=$version . || exit 1
+	ch-image push $tag || exit 1
+	ch-image reset || exit 1
 done
