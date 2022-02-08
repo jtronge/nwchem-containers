6e5b83be4bdd32edfa88ca38d1a9ed08564a6320
Jake Tronge <jtronge3@gmail.com>
2022-02-08 17:03:39 -0500

    Add NUMJOBS arg to container

--- build-all.sh
+++ build-all.sh
@@ -4,7 +4,8 @@ VERSIONS="519b710bd09ebe3c10703293093d3cf70ce44333 05aafc87223af82f58865d8b0f924
 cd nwchem-lanl
 for version in $VERSIONS; do
 	tag=jtronge/nwchem:`printf $version | head -c 7`
-	ch-image build --force -t $tag --build-arg VERSION=$version . || exit 1
+	ch-image build --force -t $tag --build-arg VERSION=$version \
+		--build-arg NUMJOBS=4 . || exit 1
 	ch-image push $tag || exit 1
 	ch-image reset || exit 1
 done
